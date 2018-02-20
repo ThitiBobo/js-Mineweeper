405e98c5e0ccd446858805c110ff0b651bb0f0e7
Thibaut Delplanque <t.delplanque92@gmail.com>
2018-02-20 09:24:04 +0100

    demineur march, ajout d'une IA

--- sketch.js
+++ sketch.js
@@ -13,11 +13,11 @@ var cols;
 var rows;
 var w = 30;
 
-var totalBees = 40;
+var totalBees = 30;
 
 function setup(){
 
-   createCanvas(401,401)
+   createCanvas(801,801)
    cols = floor(width / w);
    rows = floor(height / w);
    grid = make2DArray(cols,rows);
@@ -33,6 +33,7 @@ function setup(){
       var j = floor(random(rows));
       if (grid[i][j].bee == false) {
          grid[i][j].bee = true;
+         grid[i][j].neighborCount = -1;
          n++;
       }
    }
@@ -53,10 +54,46 @@ function setup(){
    }
 }
 
+function reveal0Zone(i,j){
+   for (var xoff = -1; xoff <= 1; xoff++) {
+      for (var yoff = -1; yoff <= 1; yoff++) {
+         var x = i + xoff;
+         var y = j + yoff;
+         if (x > -1 && x < cols && y > -1 && y< rows) {
+            if (grid[x][y].revealed == false) {
+               grid[x][y].reveal();
+               if (grid[x][y].neighborCount == 0) {
+                  reveal0Zone(x,y);
+               }
+            }
+         }
+
+      }
+   }
+}
+
+function gameOver(){
+   for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+         grid[i][j].reveal();
+      }
+   }
+}
+
 function mousePressed(){
    for (var i = 0; i < cols; i++) {
       for (var j = 0; j < rows; j++) {
          if (grid[i][j].contains(mouseX, mouseY)){
+            if (grid[i][j].revealed == false) {
+               if (grid[i][j].bee == true) {
+                  gameOver();
+                  return;
+               }
+               if (grid[i][j].neighborCount == 0) {
+                  reveal0Zone(i,j);
+                  return;
+               }
+            }
             grid[i][j].reveal();
          }
       }
